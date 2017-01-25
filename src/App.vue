61f8fd5b95328449892d4d81f20038aaacf10344
dianyingx007 <dianyingx007@163.com>
2017-01-25 17:36:23 +0800

    完成：添加todo事件，改变todo事件的状态

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello></hello>
+    <h1 v-html='title'></h1>
+    <h2>{{title2}}</h2>
+    <input v-model='newItem' v-on:keyup.enter='addItem'>
+    <ul>
+      <li v-for='Item in Items' v-on:click='changeFinished(Item)'>
+        <p v-bind:class='[bold,{finished:Item.isFinished}]'>{{Item.name}}</p>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
-
 export default {
   name: 'app',
-  components: {
-    Hello
+  data () {
+    return {
+      title: '<span>?</span>this is a todolist',
+      title2: '<p>todo:</p>',
+      newItem: '',
+      Items: [{
+        name: 'apple',
+        isFinished: false
+      },
+      {
+        name: 'banana',
+        isFinished: true
+      }],
+      bold: 'bold700'
+    }
+  },
+  methods: {
+    addItem: function () {
+      this.Items.push({
+        name: this.newItem,
+        isFinished: false
+      })
+      this.newItem = ''
+    },
+    changeFinished: function (Item) {
+      Item.isFinished = !Item.isFinished
+    }
   }
 }
 </script>
@@ -24,5 +54,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.finished {
+  color: red;
+}
+.bold700 {
+  font-weight: bold;
 }
 </style>
