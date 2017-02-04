52625401720698aaa0ff72f3b8f97a08c6d8418b
danny <dianyingx007@163.com>
2017-02-04 17:50:57 +0800

    修复bug：当前无存储时，要转为空数组

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ const STORAGE_KEY = 'todo_items'
 
 export default {
   fetch () {
-    return JSON.parse(window.localStorage.getItem(STORAGE_KEY))
+    return JSON.parse(window.localStorage.getItem(STORAGE_KEY)) || []
   },
   store (items) {
     window.localStorage.setItem(STORAGE_KEY, JSON.stringify(items))
